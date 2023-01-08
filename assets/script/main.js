7bee33b4818e91837b8408417cecd11217892b07
tom-2810 <tom.poortman@hva.nl>
2023-01-08 18:00:15 +0100

    rotate door scrollwheel

--- assets/script/main.js
+++ assets/script/main.js
@@ -10,6 +10,8 @@ let user = document.querySelector('a:nth-of-type(9)');
 let interface = document.querySelector('a:nth-of-type(10)');
 let interaction = document.querySelector('a:nth-of-type(12)');
 
+var rotateDegrees = 0;
+
 frontend.addEventListener('click', () => errorHandler(frontend));
 frontend.addEventListener('animationend', () => errorHandler(frontend));
 
@@ -19,12 +21,22 @@ design.addEventListener('animationend', () => focusHandler(design));
 interaction.addEventListener('click', () => jumpHandler(interaction));
 interaction.addEventListener('animationend', () => jumpHandler(interaction));
 
+document.addEventListener('wheel', function (e) {
+  rotateDegrees += e.deltaY * .6;
+  rotateHandler(and, rotateDegrees);
+});
+
 function errorHandler(element) {
   element.classList.toggle('error');
 }
 
 function focusHandler(element) {
   element.classList.toggle('focus');
+}
+
+function rotateHandler(element, degrees) {
+  element.classList.add('rotate');
+  element.style.setProperty('--degrees', degrees + "deg");
 }
 
 function jumpHandler(element) {
