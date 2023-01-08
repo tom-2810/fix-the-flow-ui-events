426895e60aaa72d8517eef979bc7421a9d158062
tom-2810 <tom.poortman@hva.nl>
2023-01-08 22:05:15 +0100

    backflip, gradient, poof!

--- assets/script/main.js
+++ assets/script/main.js
@@ -3,7 +3,7 @@ let design = document.querySelector('a:nth-of-type(2)');
 let and = document.querySelector('a:nth-of-type(3)');
 let development = document.querySelector('a:nth-of-type(4)');
 let sprint5 = document.querySelector('a:nth-of-type(5)');
-let fix = document.querySelector('a:nth-of-type(6)');
+let fix = document.querySelector('.fix a');
 let the = document.querySelector('a:nth-of-type(7)');
 let flow = document.querySelector('a:nth-of-type(8)');
 let user = document.querySelector('a:nth-of-type(9)');
@@ -11,6 +11,8 @@ let interface = document.querySelector('a:nth-of-type(10)');
 let interaction = document.querySelector('a:nth-of-type(12)');
 
 var rotateDegrees = 0;
+
+document.addEventListener('keydown', checkKeyPressed);
 
 frontend.addEventListener('click', () => errorHandler(frontend));
 frontend.addEventListener('animationend', () => errorHandler(frontend));
@@ -20,6 +22,12 @@ design.addEventListener('animationend', () => focusHandler(design));
 
 interaction.addEventListener('click', () => jumpHandler(interaction));
 interaction.addEventListener('animationend', () => jumpHandler(interaction));
+
+development.addEventListener('click', () => flipHandler(development));
+development.addEventListener('animationend', () => flipHandler(development));
+
+fix.addEventListener('click', () => poofHandler(fix));
+fix.addEventListener('animationend', () => poofHandler(fix));
 
 document.addEventListener('wheel', function (e) {
   rotateDegrees += e.deltaY * .6;
@@ -34,6 +42,15 @@ function focusHandler(element) {
   element.classList.toggle('focus');
 }
 
+function flipHandler(element) {
+  element.classList.toggle('flip');
+}
+
+function poofHandler(element) {
+  element.classList.add('poof');
+  document.querySelector('.fix div').classList.add('poof-message');
+}
+
 function rotateHandler(element, degrees) {
   element.classList.add('rotate');
   element.style.setProperty('--degrees', degrees + "deg");
@@ -41,4 +58,13 @@ function rotateHandler(element, degrees) {
 
 function jumpHandler(element) {
   element.classList.toggle('jump');
+}
+
+function checkKeyPressed(e) {
+  switch (e.keyCode) {
+    case 32:
+      document.querySelectorAll('a').forEach(function (element) {
+        element.classList.add('gradient');
+      })
+  }
 }
