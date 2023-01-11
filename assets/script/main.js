b7402c8d6c3ecfcc338357199ef7970bfe10f49e
tom-2810 <tom.poortman@hva.nl>
2023-01-11 13:34:15 +0100

    grow en fly

--- assets/script/main.js
+++ assets/script/main.js
@@ -4,14 +4,15 @@ let and = document.querySelector('a:nth-of-type(3)');
 let development = document.querySelector('a:nth-of-type(4)');
 let sprint5 = document.querySelector('a:nth-of-type(5)');
 let fix = document.querySelector('.fix a');
-let the = document.querySelector('a:nth-of-type(7)');
-let flow = document.querySelector('a:nth-of-type(8)');
-let user = document.querySelector('a:nth-of-type(9)');
-let interface = document.querySelector('a:nth-of-type(10)');
+let the = document.querySelector('a:nth-of-type(6)');
+let flow = document.querySelector('a:nth-of-type(7)');
+let user = document.querySelector('a:nth-of-type(8)');
+let interface = document.querySelector('a:nth-of-type(9)');
+let events = document.querySelector('a:nth-of-type(10)')
 let interaction = document.querySelector('a:nth-of-type(11)');
+var allActive = document.querySelectorAll(".active");
 
 var rotateDegrees = 0;
-
 
 document.addEventListener('keydown', checkKeyPressed);
 
@@ -30,10 +31,18 @@ development.addEventListener('animationend', () => flipHandler(development));
 fix.addEventListener('click', () => poofHandler(fix));
 fix.addEventListener('animationend', () => poofHandler(fix));
 
+sprint5.addEventListener('click', colorFillHandler);
+
+events.addEventListener('mousedown', () => flyHandler(events));
+events.addEventListener('animationend', () => flyHandler(events))
+
 document.addEventListener('wheel', function (e) {
   rotateDegrees += e.deltaY * .6;
   rotateHandler(and, rotateDegrees);
 });
+
+interface.addEventListener('dblclick', () => dblclickHandler(interface));
+interface.addEventListener('animationend', () => dblclickHandler(interface));
 
 function errorHandler(element) {
   element.classList.toggle('error');
@@ -61,6 +70,10 @@ function jumpHandler() {
   interaction.classList.toggle('jump')
 }
 
+function flyHandler(element) {
+  element.classList.toggle('fly');
+}
+
 function checkKeyPressed(e) {
   switch (e.keyCode) {
     case 32:
@@ -68,4 +81,20 @@ function checkKeyPressed(e) {
         element.classList.toggle('gradient');
       })
   }
+}
+
+function dblclickHandler(element) {
+  element.classList.toggle('grow');
+}
+
+function colorFillHandler(element) {
+  const colorBackground = document.querySelector(".colorBackground");
+
+  colorBackground.style.backgroundColor = randomColor();
+  colorBackground.classList.toggle("fill");
+}
+
+function randomColor() {
+  const randomColor = Math.floor(Math.random()*16777215).toString(16);
+  return `#${randomColor}`;
 }
